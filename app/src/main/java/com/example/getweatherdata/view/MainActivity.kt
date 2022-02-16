ee53370215df4fa2fe89530cc1d9396b74b06d0a
natura1ie <naturalie.pt@gmail.com>
2022-02-16 21:26:23 +0900

    initial commit

---
+++ app/src/main/java/com/example/getweatherdata/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.getweatherdata.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.getweatherdata.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
