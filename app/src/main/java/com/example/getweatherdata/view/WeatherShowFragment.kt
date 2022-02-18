ecd61fcb693b5297c83e3b1529a8c52760db9d9d
natura1ie <naturalie.pt@gmail.com>
2022-02-18 16:16:54 +0900

    URL通信処理をViewModelに移動

--- app/src/main/java/com/example/getweatherdata/view/WeatherShowFragment.kt
+++ app/src/main/java/com/example/getweatherdata/view/WeatherShowFragment.kt
@@ -13,14 +13,9 @@ import com.example.getweatherdata.R
 import com.example.getweatherdata.databinding.WeatherShowMainBinding
 import com.example.getweatherdata.viewmodel.WeatherShowViewModel
 import kotlinx.coroutines.*
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.URL
 
 open class WeatherShowFragment : Fragment() {
     private val weatherShowViewModel: WeatherShowViewModel by viewModels()
-    private var weather = ""
     open lateinit var binding: WeatherShowMainBinding
 
     override fun onCreateView(
@@ -48,34 +43,11 @@ open class WeatherShowFragment : Fragment() {
         // 非同期処理が終わるのを待ってUIスレッドで画面更新を行う必要があるためハンドラーを用意
         val handler = Handler(Looper.getMainLooper())
         withContext(Dispatchers.Default) {
-            // 取得した自分のAPIキーを入力
-            val apiKEY = "(TODO:APIキーを入力)"
-            // 取得したい都道府県の緯度と経度を入力
-            val lat = "(TODO:緯度を入力)"
-            val lon = "(TODO:経度を入力)"
-
-            // 天気情報を取得
-            val url =
-                URL("https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&lang=ja&units=metric&APPID=$apiKEY")
-            val readData = BufferedReader(InputStreamReader(url.openStream()))
-            val json = JSONObject(readData.readText())
-            val list = json.getJSONArray("list")
-            for (i in 0 until list.length()) {
-                val weatherList = list.getJSONObject(i)
-                val weatherObject = weatherList.getJSONArray("weather").getJSONObject(0)
-                // 日時を取得
-                val dtText = weatherList.getString("dt_txt")
-                // 天気を取得
-                val descriptionText = weatherObject.getString("description")
-                weather += "$dtText〜$descriptionText\n"
-                withContext(Dispatchers.Default) {
-                    binding.viewModel!!.setWeatherData(weather)
-                }
-            }
+            binding.viewModel!!.callWeatherData()
         }
         handler.post {
             // 画面を更新する
-            binding.weatherData.text = binding.viewModel!!.getWeatherData()
+            binding.weatherData.text = binding.viewModel!!.weatherData.value
         }
     }
 }
